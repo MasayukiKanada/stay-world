24a142aecbcfe0cb56dcd76f6eda5b93298021cb
MasayukiKanada <kanada.masayuki.hata@gmail.com>
2025-04-14 09:24:41 +0900

    travel

--- wp-content/themes/SiMPLE/page-front.php
+++ wp-content/themes/SiMPLE/page-front.php
@@ -27,6 +27,25 @@ get_header(); ?>
                 </div>
             </div>
         </div>
+        <div class="travel">
+            <div class="travel__inner">
+                <div class="travel__item">
+                    <div class="travel__img">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/image-travel.jpg" alt="Travel the World">
+                    </div>
+                    <div class="travel__texts">
+                        <h2 class="travel__title main-title"><span class="purple">Travel</span> the World</h2>
+                        <div class="travel__sub-title sub-title">世界中を旅しよう</div>
+                        <div class="travel__txt">
+                            <p>世界１５０ヶ国で<br>利用できる民泊サービスです。</p><p>あなたの望む宿がきっと見つかります。</p>
+                        </div>
+                        <div class="travel__btn">
+                            <button class="btn filled">もっと詳しく</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
 <?php get_footer(); ?>
